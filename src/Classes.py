bf7500aaccf3ab9a3d941c7663646bfd93052344
Marvin <captainpismire@gmail.com>
2021-10-11 23:10:49 -0700

    spreadhseets dumped for BS and BD

--- src/Classes.py
+++ src/Classes.py
@@ -1,8 +1,9 @@
 import os
 import zlib
-import sys
+import xlrd
+import xlwt
+import math
 # import pudb; pu.db
-import struct
 
 class FILE:
     def __init__(self, data):
@@ -36,11 +37,210 @@ class FILE:
         return string
 
 
+# FILE object + access to reading and patching as if a spreadsheet
+class DATAFILE(FILE):
+    def __init__(self, data):
+        super().__init__(data)
+        self.address = 8
+        # Data
+        self.base = self.read()
+        self.size = self.read()
+        # Command strings
+        self.comBase = self.read()
+        self.comSize = self.read()
+        # Text
+        self.textBase = self.read()
+        self.textSize = self.read()
+        # Entries
+        self.stride = self.read() # bytes / entry
+        self.count = self.read()  # number of entries
+
+    def getTextData(self):
+        data = self.data[self.textBase:self.textBase+self.textSize]
+        data = bytes(data[::2]).split(b'\x00')
+        y = len(data) // self.count
+        x = [[str(d)[2:] for d in data[i::y]] for i in range(y)]
+        return x
+
+    def getComData(self):
+        data = self.data[self.comBase:self.comBase+self.comSize]
+        x = bytes(data).split(b'\x00')
+        try:
+            return [xi.decode() for xi in x]
+        except:
+            return []
+        
+    # Data tables can only be updated under certain circumstances.
+    # Assumes all columns have the same byte size (currently 4)
+    # Assumes no text.
+    # Now, only use for Shops and Ability tables
+    def updateData(self, *cols):
+        # Ensure no text
+        assert self.comSize == 0
+        assert self.textSize == 0
+        # Update data
+        data = bytearray([])
+        numEntries = 0
+        for row in zip(*cols):
+            for ri in row:
+                data += ri.to_bytes(4, byteorder='little', signed=True)
+            numEntries += 1
+        lenData = len(data).to_bytes(4, byteorder='little', signed=True)
+        fileSize = int(len(data)+0x30).to_bytes(4, byteorder='little', signed=True)
+        header = bytearray(b'BTBF')
+        header += fileSize
+        header += int(0x30).to_bytes(4, byteorder='little', signed=True)
+        header += lenData
+        header += fileSize + bytearray([0]*4)
+        header += fileSize + bytearray([0]*4)
+        header += int(8).to_bytes(4, byteorder='little', signed=True)
+        header += numEntries.to_bytes(4, byteorder='little', signed=True)
+        header += bytearray([0]*8)
+        self.data = header + data
+
+    def readCol(self, col, row=0, numRows=None):
+        if not numRows:
+            numRows = self.count
+        numRows = min(numRows, self.count - row)
+        data = []
+        for r in range(row, row+numRows):
+            data.append( self.readValue(r, col) )
+        return data
+
+    def readRow(self, row, col=0, numCol=None):
+        if not numCol:
+            maxCol = int(self.stride / 4)
+            numCol = maxCol - col
+        data = []
+        for c in range(col, col+numCol):
+            data.append( self.readValue(row, c) )
+        return data
+    
+    def readValue(self, row, col, size=4):
+        address = self.base + row*self.stride + col*size
+        return int.from_bytes(self.data[address:address+size], byteorder='little', signed=True)
+
+    def patchCol(self, lst, col, row=0):
+        for i, value in enumerate(lst):
+            self.patchValue(value, row+i, col)
+
+    def patchRow(self, lst, row, col=0):
+        for i, value in enumerate(lst):
+            self.patchValue(value, row, col+i)
+
+    def patchValue(self, value, row, col, size=4):
+        address = self.base + row*self.stride + col*size
+        self.data[address:address+size] = value.to_bytes(size, byteorder='little', signed=True)
+    
+    def readComString(self, row, col):
+        offset = self.readValue(row, col)
+        self.address = self.comBase + offset
+        return self.readString(size=1)
+
+    def readTextString(self, row, col):
+        offset = self.readValue(row, col)
+        self.address = self.textBase + offset
+        return self.readString(size=2)
+
+    def readTextStringAll(self, col):
+        strings = []
+        for row in range(self.count):
+            string = self.readTextString(row, col)
+            strings.append(string)
+        return strings
+    
+    # TODO
+    def patchString(self):
+        pass
+
+    # TODO -- probably won't need to patch commands, only text
+    def patchTextString(self, string, row, col):
+        # Check string first to ensure it fits!
+        check = self.readTextString(row, col)
+        assert len(check) >= len(string), 'String is too long!'
+        sizeDiff = 2 * (len(check) - len(string))
+        # Encode string
+        newString = string.encode('utf-16')[2:]
+        newString += bytearray([0]*sizeDiff)
+        # Patch
+        offset = self.readValue(row, col)
+        address = self.textBase + offset
+        self.data[address:address+len(newString)] = newString
+        
+
+
+class TABLE:
+    def __init__(self, fileName):
+        self.fileName = fileName
+        with open(self.fileName, 'rb') as file:
+            self.tableData = bytearray(file.read())
+        baseName = os.path.basename(self.fileName)
+        self.crowdFiles = {baseName: DATAFILE(self.tableData)}
+
+    def dumpSheet(self):
+        sheetName = self.fileName.split('.')[0] + '.xlsx'
+        wb = xlwt.Workbook()
+        for file in self.crowdFiles:
+            if file == '.fscache':
+                continue
+
+            x = file.replace('_', ' ')
+            if len(x) > 31:
+                x = x[:31]
+            wb.add_sheet(x)
+            sheet = wb.get_sheet(x)
+
+            data = self.crowdFiles[file]
+            com = data.getComData()
+            textData = data.getTextData()
+            numCol = int(data.stride / 4)
+            numRows = data.count
+
+            # TEXT STUFF
+            comCols = int(round(len(com) / numRows))
+            if comCols == 0 and com:
+                for c in com:
+                    assert c == ''
+                comCols = len(com)
+
+            col = 0
+            row = 1
+            counter = 0
+            while com:
+                for i in range(comCols):
+                    if com:
+                        value = com.pop(0)
+                        sheet.write(row, col+i, value)
+                row += 1
+                counter += 1
+                if counter == 10000:
+                    print('STUCK IN LOOP IN ', file, sheetName)
+            col += max(1, comCols)
+
+            for text in textData:
+                for row, value in enumerate(text):
+                    sheet.write(row+1, col, value)
+                col += 1
+
+            for i in range(numCol):
+                colData = data.readCol(i)
+                for row, value in enumerate(colData):
+                    sheet.write(row+1, col, value)
+                col += 1
+
+            pre, ext = os.path.splitext(self.fileName)
+            output = pre + '.xlsx'
+            wb.save(output)
+            
+
+    def dump(self):
+        with open(self.fileName, 'wb') as file:
+            file.write(self.tableData)
+
+
+
 class CROWD:
     def __init__(self, path):
-        if path is None:
-            return
-
         self.path = path
         
         fileName = os.path.join(path, 'index.fs')
@@ -49,68 +249,33 @@ class CROWD:
             self.indexFile = FILE(self.indexData)
 
         fileName = os.path.join(path, 'crowd.fs')
-        if os.path.exists(fileName):
-            with open(fileName, 'rb') as file:
-                self.crowdData = bytearray(file.read())
-            # Split crowd files
-            self.crowdFiles = {}
-            self.separateCrowd()
-        else:
-            # Load files
-            self.crowdFiles = {}
-            self.loadFiles()
+        with open(fileName, 'rb') as file:
+            self.crowdData = bytearray(file.read())
 
-    def dumpCrowd(self, outpath):
+        # Split crowd files
+        self.isCompressed = {}
+        self.crowdFiles = {}
+        self.separateCrowd()
+
+    def dump(self):
         # Rebuild index and crowd data
         self.joinCrowd()
-        # Make directory
-        if not os.path.isdir(outpath):
-            os.makedirs(outpath)
         # Dump index
-        fileOut = os.path.join(outpath, 'index.fs')
+        fileOut = os.path.join(self.path, 'index.fs')
         with open(fileOut, 'wb') as file:
             file.write(self.indexData)
         # Dump crowd
-        fileOut = os.path.join(outpath, 'crowd.fs')
+        fileOut = os.path.join(self.path, 'crowd.fs')
         with open(fileOut, 'wb') as file:
             file.write(self.crowdData)
 
     def dumpFiles(self, outpath):
-        if 'romfs' in self.path:
-            base = self.path.split('romfs')[-1]
-        elif 'RomFS' in self.path:
-            base = self.path.split('RomFS')[-1]
-        else:
-            sys.exit("Folder must be romfs or RomFS")
-
-        # remove "." and "/"
-        while base and not (base[0].isdigit() or base[0].isalpha()):
-            base = base[1:]
-            
-        path = os.path.join(outpath, base)
-        if not os.path.isdir(path):
-            os.makedirs(path)
+        if not os.path.isdir(self.path):
+            os.makedirs(self.path)
         for fileName, data in self.crowdFiles.items():
-            fileOut = os.path.join(path, fileName)
+            fileOut = os.path.join(self.path, fileName)
             with open(fileOut, 'wb') as file:
                 file.write(data.data)
-
-    def loadFiles(self):
-        self.indexFile.address = 0
-        nextAddr = self.indexFile.read()
-        while True:
-            self.indexFile.address += 0xc
-            fileName = self.indexFile.readString()
-            fullName = os.path.join(self.path, fileName)
-            if os.path.exists(fullName):
-                with open(fullName, 'rb') as file:
-                    self.crowdFiles[fileName] = FILE(bytearray(file.read()))
-            else:
-                sys.exit(f"{fileName} does not exist in {self.path}")
-            if nextAddr == 0:
-                break
-            self.indexFile.address = nextAddr
-            nextAddr = self.indexFile.read()
 
     def separateCrowd(self):
         nextAddr = self.indexFile.read()
@@ -160,23 +325,81 @@ class CROWD:
         self.crowdData = self.adjustSize(self.crowdData)
 
     def extractFile(self, fileName, base, size):
-        data = zlib.decompress(self.crowdData[base+4:base+size], -15)
-        data = bytearray(data)
-        return FILE(data)
+        self.isCompressed[fileName] = self.crowdData[base] == 0x60
+        if self.isCompressed[fileName]:
+            decompSize = int.from_bytes(self.crowdData[base+1:base+4], byteorder='little', signed=True)
+            try: # On the off chance the file starts with 0x60 but is not compressed!
+                data = zlib.decompress(self.crowdData[base+4:base+size], -15)
+                data = bytearray(data)
+                assert len(data) == decompSize
+            except:
+                self.isCompressed[fileName] = False
+        if not self.isCompressed[fileName]:
+            data = self.crowdData[base:base+size]
+        return DATAFILE(data)
+        # self.isCompressed[fileName] = self.crowdData[base] & 0xFF  == 0x60
+        # if self.isCompressed[fileName]:
+        #     data = zlib.decompress(self.crowdData[base+4:base+size], -15)
+        #     data = bytearray(data)
+        # else:
+        #     data = self.crowdData[base:base+size]
+        # return DATAFILE(data)
 
     def getData(self, fileName):
-        size = len(data)
-        data = zlib.compress(data)[2:-4]
-        header = int((size << 8) + 0x60).to_bytes(4, byteorder='little')
-        return header + data
+        data = self.crowdFiles[fileName].data
+        if self.isCompressed[fileName]:
+            size = len(data)
+            data = zlib.compress(data)[2:-4]
+            header = int((size << 8) + 0x60).to_bytes(4, byteorder='little')
+            data = header + data
+        return data
 
-class CROWD_BD(CROWD):
-    def __init__(self, path):
-        super().__init__(path)
+    def dumpSheet(self):
+        wb = xlwt.Workbook()
+        for file in self.crowdFiles:
+            if file == '.fscache':
+                continue
+            # x = os.path.basename(self.fileName).replace('_', ' ')
+            x = file.replace('_', ' ')
+            if len(x) > 31:
+                x = x[:31]
+            wb.add_sheet(x)
+            sheet = wb.get_sheet(x)
 
-    def extractFile(self, fileName, base, size):
-        data = self.crowdData[base:base+size]
-        return FILE(data)
+            data = self.crowdFiles[file]
+            com = data.getComData()
+            textData = data.getTextData()
+            numCol = int(data.stride / 4)
+            numRows = data.count
 
-    def getData(self, fileName):
-        return self.crowdFiles[fileName].data
+            # TEXT STUFF
+            comCols = int(round(len(com) / numRows))
+            if comCols == 0 and com:
+                for c in com:
+                    assert c == ''
+                comCols = len(com)
+
+            col = 0
+            row = 1
+            while com:
+                for i in range(comCols):
+                    if com:
+                        value = com.pop(0)
+                        sheet.write(row, col+i, value)
+                row += 1
+            col += max(1, comCols)
+
+            for text in textData:
+                for row, value in enumerate(text):
+                    sheet.write(row+1, col, value)
+                col += 1
+
+            for i in range(numCol):
+                colData = data.readCol(i)
+                for row, value in enumerate(colData):
+                    sheet.write(row+1, col, value)
+                col += 1
+
+        sheetName = os.path.join(self.path, 'crowd.xlsx')
+        wb.save(sheetName)
+            
--- src/ROM.py
+++ src/ROM.py
@@ -1,7 +1,9 @@
-from Classes import CROWD, CROWD_BD
+# from Classes import CROWD, CROWD_BD
+from Classes import CROWD, TABLE
 import os
 import shutil
 import sys
+# from Spreadsheets import XLSX
 
 class ROM:
     def __init__(self, settings):
@@ -9,21 +11,33 @@ class ROM:
         self.game = settings['game']
         self.pathIn = self.settings['rom']
         if os.path.isdir(self.pathOut):
+            return
             shutil.rmtree(self.pathOut)
         os.mkdir(self.pathOut)
 
     def fail(self):
         shutil.rmtree(self.pathOut)
 
+    # Not sure why I did all these copies?
     def loadCrowd(self, path):
-        src = os.path.join(self.pathIn, path)
-        if self.game == 'BD':
-            obj = CROWD_BD(src) # No decompression/compression
-        elif self.game == 'BS':
-            obj = CROWD(src) # Decompress/compresses all crowds
-        else:
-            sys.exit(f"Game setting must be BD or BS, not {self.game}")
-        return obj
+        dest = os.path.join(self.pathOut, path)
+        if not os.path.isdir(dest):
+            os.makedirs(dest)
+        src = os.path.join(self.pathIn, path, 'crowd.fs')
+        shutil.copy(src, dest)
+        src = os.path.join(self.pathIn, path, 'index.fs')
+        shutil.copy(src, dest)
+        return CROWD(dest)
+
+    # Not sure why I did all these copies?
+    def loadTable(self, fileName):
+        src = os.path.join(self.pathIn, fileName)
+        dest = os.path.join(self.pathOut, fileName)
+        base = os.path.dirname(dest)
+        if not os.path.isdir(base):
+            os.makedirs(base)
+        shutil.copy(src, dest)
+        return TABLE(dest)
 
     def copyFile(self, fileName):
         src = os.path.join(self.pathIn, fileName)
@@ -32,6 +46,16 @@ class ROM:
         if not os.path.isdir(base):
             os.makedirs(base)
         shutil.copy(src, dest)
+
+    # def dumpSpreadsheet(self, fileName):
+    #     src = os.path.join(self.pathIn, fileName)
+    #     dest = os.path.join(self.pathOut, fileName)
+    #     base = os.path.dirname(dest)
+    #     if not os.path.isdir(base):
+    #         os.makedirs(base)
+    #     sheet = XLSX(src)
+    #     sheet.loadData()
+    #     sheet.dumpSheet(dest)
         
 
 class PACK(ROM):
@@ -64,18 +88,38 @@ class UNPACK(ROM):
     def __init__(self, settings):
         self.pathOut = os.path.join(os.getcwd(), f"romfs_unpacked")
         super().__init__(settings)
-
-        obj = CROWD(None)
-
         dir = os.getcwd()
         os.chdir(self.pathIn)
         for root, dirs, files in os.walk('.'):
             root = root[2:]
             for file in files:
+                if file == 'index.fs':
+                    continue
+                fileName = os.path.join(root, file)
+                checkName = os.path.join(self.pathOut, fileName)
+                if os.path.isfile(checkName):
+                    continue
+                if 'Script/Scene/TW_13_FlowerCountry/Scene/crowd.fs' == fileName:
+                    print('DECOMPRESS ERROR UP NEXT!')
                 if file == 'crowd.fs':
-                    crowd = self.loadCrowd(root)
-                    crowd.dumpFiles(self.pathOut)
-                else: # Copies index.fs and any other files
-                    fileName = os.path.join(root, file)
+                    table = self.loadCrowd(root)
+                    table.dumpFiles(self.pathOut)
                     self.copyFile(fileName)
+                else:
+                    table = self.loadTable(fileName)
+                    self.copyFile(fileName)
+                # if 'Graphics' in fileName:
+                #     continue
+
+                ### OKAY FILES:
+                # btb
+                # tbl
+                if file == 'crowd.fs' or fileName.split('.')[-1] in ['btb', 'tbl']:
+                    try:
+                        print(fileName)
+                        table.dumpSheet()
+                    except:
+                        print(fileName, 'failed!')
+                        # sys.exit()
+                
         os.chdir(dir)
